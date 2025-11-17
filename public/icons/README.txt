c9f552a991c4dd164f6ecf7f5e67b496cc8fc276
ItsRamanRathore <ramanrathore031204@gmail.com>
2025-11-17 17:17:00 +0530

    Add PWA support - installable web app

---
+++ public/icons/README.txt
@@ -0,0 +1,2 @@
+// Placeholder - Icons will be generated
+// Use the icon.svg or generate-icons.html to create icon-192.png and icon-512.png
---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'delhibreathe-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/styles.css'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate service worker and clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch strategy: Network first, fallback to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone response and cache it
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // If network fails, try cache
+        return caches.match(event.request);
+      })
+  );
+});
